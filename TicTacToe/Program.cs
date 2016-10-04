6fd5c0df802aef84761cbe523c17d0d9305a4551
Damien Maya <damien_maya@yahoo.com>
2016-10-04 15:55:10 -0500

    solved problem 1

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,8 +8,71 @@ namespace TicTacToe
 {
     class Program
     {
+
+        static string[] board = { " ", " ", " ",
+                                  " ", " ", " ",
+                                  " ", " ", " " };
+
         static void Main(string[] args)
         {
+            Console.WriteLine("> Let's play a game.");
+            PrintBoard();
+            Play();
+            Console.ReadLine();
+        }
+
+        static void Play()
+        {
+            Console.WriteLine("> Enter a spot. \"x,y\"");
+
+            char[] delim = { ',' };
+            string[] positions = Console.ReadLine().Split(delim); // --> "1,2" -> ["1", "2"]
+            int x = Int32.Parse(positions[0]);
+            int y = Int32.Parse(positions[1]);
+            int index = GetIndex(x, y);
+            if (index >= 0 && index<=8 )
+            {
+                
+                board[index] = "X";
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+
+                Play();
+                
+
+            }
+            
+
+            PrintBoard();
+            
+
+
+            Play();
+        }
+
+        static int GetIndex(int x, int y)
+        {
+            return (x - 1) + (y - 1) * 3;
+        }
+
+        static void PrintBoard()
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine("| {0} | {1} | {2} |", board[0], board[1], board[2]);
+            Console.WriteLine("| {0} | {1} | {2} |", board[3], board[4], board[5]);
+            Console.WriteLine("| {0} | {1} | {2} |", board[6], board[7], board[8]);
+            Console.WriteLine("---------");
+        }
+
+        static void PromptForInput()
+        {
+
+        
+
+
+
         }
     }
 }
